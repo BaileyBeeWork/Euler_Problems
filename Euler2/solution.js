ee3cb722a92ce8c6c8e977178dff1ab5005d1b6d
BaileyBeeWork <133674213+BaileyBeeWork@users.noreply.github.com>
2024-02-23 16:48:51 -0600

    removed stupid comments, formatted code

--- Euler2/solution.js
+++ Euler2/solution.js
@@ -1,19 +1,18 @@
 function Euler2() {
-  
-    var last_term = 0;      //x
-    var current_term = 1;   //y
-    var next_term = 0;          //z
-    var result = 0;
+  var last_term = 0;
+  var current_term = 1;
+  var next_term = 0;
+  var result = 0;
 
-    while (current_term < 4000000){
-        next_term = last_term + current_term; //z = x + y
-        last_term = current_term;  //x = y
-        current_term = next_term; //y = z
-        if(current_term % 2 === 0){
-            result += current_term;
-        }
+  while (current_term < 4000000) {
+    next_term = last_term + current_term;
+    last_term = current_term;
+    current_term = next_term;
+    if (current_term % 2 === 0) {
+      result += current_term;
     }
-    return result;
+  }
+  return result;
 }
 
 console.log(Euler2());
